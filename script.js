377768e40aeb6fca764b25ed24c13a2534492d15
Volcomix <m.santo13@hotmail.com>
2023-08-26 13:33:39 -0600

    updated variables to an object

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Assignment code here
 //Variables
-var characterLength = []
-var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I","J", "K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialCharacters = ["!","@","#","$","%","^","&","*","?"];
-var number = ["0","1","2","3","4","5","6","7","8","9"];
+const characters = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()"
+};
 
 
 //Prompts for user
@@ -13,7 +14,11 @@ function prompts() {
 
   var characterLength = parseInt(prompt(" Please choose lenght of password, in numerical form, from 8-128")); 
     if(characterLength <8 || characterLength >128) {
-      alert("Please choose a number between 8-128");
+      return "Password must be between 8-128 characters long";
+    } else if (isNaN(characterLength)) {
+      characterLength = prompt("Please enter a valid number");
+    } else {
+      alert("Password will be" + characterLength + "characters long");
     }
   
   var uppercaseCharacters = confirm("Include uppercase letters?");
